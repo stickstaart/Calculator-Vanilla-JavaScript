0b71af274ceb8bc841080160d165d5411b8fc70b
[Joz] <[jzvndk@gmail.com]>
2022-05-30 17:15:44 +0200

    added functionality: equals button, add, subtract, divide and multiply now work as intended

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const operands = Array.from(document.querySelectorAll(`.operand`));
 const actions = Array.from(document.querySelectorAll(`.action`));
 const displayTop = document.querySelector(`.display-top-value`);
 const displayBottom = document.querySelector(`.display-bottom-value`);
+const equalsBtn = document.querySelector(`.equals`);
 
 numbers.map((number) => {
   number.addEventListener(`click`, (e) => {
@@ -14,38 +15,6 @@ numbers.map((number) => {
 operands.map((operand) => {
   operand.addEventListener(`click`, (e) => {
     console.log(e.target.innerText);
-    switch (e.target.innerText) {
-      case "=":
-        switch (displayTop.innerText[displayTop.innerText.length - 1]) {
-          case "+": {
-            console.log("I need to add two numbers");
-            displayTop.innerText = "";
-            displayBottom.innerText = "";
-            break;
-          }
-          case "-": {
-            console.log("I need to subtract two numbers");
-            displayTop.innerText = "";
-            displayBottom.innerText = "";
-            break;
-          }
-          case "/": {
-            console.log("I need to divide two numbers");
-            displayTop.innerText = "";
-            displayBottom.innerText = "";
-            break;
-          }
-          case "x": {
-            console.log("I need to multiply two numbers");
-            displayTop.innerText = "";
-            displayBottom.innerText = "";
-            break;
-          }
-        }
-      // if (displayTop.innerText[displayTop.innerText.length - 1] === "+") {
-      //   console.log("I need to add two numbers");
-      // } else if
-    }
     displayTop.innerText = displayBottom.innerText += ` ${e.target.innerText}`;
     displayBottom.innerText = "";
   });
@@ -56,6 +25,7 @@ actions.map((action) => {
     console.log(e.target.innerText);
     switch (e.target.innerText) {
       case "AC":
+        displayTop.innerText = "";
         displayBottom.innerText = "";
         break;
       case "+/-":
@@ -67,6 +37,51 @@ actions.map((action) => {
         }
     }
   });
+});
+
+equalsBtn.addEventListener(`click`, (e) => {
+  console.log(e.target.innerText);
+  switch (displayTop.innerText[displayTop.innerText.length - 1]) {
+    case "+": {
+      console.log("I need to add two numbers");
+      let result =
+        parseInt(displayTop.innerText.slice(0, -2)) +
+        parseInt(displayBottom.innerText);
+      displayTop.innerText = "";
+      displayBottom.innerText = result;
+      break;
+    }
+    case "-": {
+      console.log("I need to subtract two numbers");
+      console.log(displayTop.innerText.slice(0, -2));
+      let result =
+        parseInt(displayTop.innerText.slice(0, -2)) -
+        parseInt(displayBottom.innerText);
+      displayTop.innerText = "";
+      displayBottom.innerText = result;
+      break;
+    }
+    case "/": {
+      console.log("I need to divide two numbers");
+      console.log(displayTop.innerText.slice(0, -2));
+      let result =
+        parseInt(displayTop.innerText.slice(0, -2)) /
+        parseInt(displayBottom.innerText);
+      displayTop.innerText = "";
+      displayBottom.innerText = result;
+      break;
+    }
+    case "x": {
+      console.log("I need to multiply two numbers");
+      console.log(displayTop.innerText.slice(0, -2));
+      let result =
+        parseInt(displayTop.innerText.slice(0, -2)) *
+        parseInt(displayBottom.innerText);
+      displayTop.innerText = "";
+      displayBottom.innerText = result;
+      break;
+    }
+  }
 });
 
 // numberBtn.map((number) => {
